9f198b2b384d7c3583cdb57a1761230a1292d25c
Marcel0909 <82092639+Marcel0909@users.noreply.github.com>
2021-04-07 18:00:08 +0200

    Add files via upload

---
+++ WŁAŚCIWY1.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int wybor;
+    cout<<"Czy chcesz aby program liczyl:"<<endl;
+    cout<<"1. Jaka cena wychodzi za kilogram"<<endl;
+    cout<<"2. Jaka cena wychodzi za litr"<<endl;
+    cout<<"3. Ile wynosi rabat oraz jaka cena bedzie po rabacie"<<endl;
+    cout<<"4. Promocje typu kup pare a dostaniesz paredziesiat % znizki"<<endl;
+    cout<<"Wybierz: 1,2,3 lub 4"<<endl;
+    cin>> wybor;
+    if(wybor==1)
+    {
+        cout<<"Wybrano program numer 1"<<endl;
+        cout<<"Ile gram ma produkt?"<<endl;
+        double gramy,cena, cenazakilogram;
+        cin>>gramy;
+        cout<<"Ile kosztuje produkt"<<endl;
+        cin>>cena;
+        cenazakilogram=(cena*1000)/gramy;
+        cout.precision(2);
+        cout<<"Cena za kilogram wynosi"<<" "<<fixed<<cenazakilogram<<"zl"<<endl;
+
+
+    }
+    else if(wybor==2)
+    {
+        cout<<"Wybrano program numer 1"<<endl;
+        cout<<"Ile mililitrow ma produkt?"<<endl;
+        double mililitry,cena, cenazalitr;
+        cin>>mililitry;
+        cout<<"Ile kosztuje produkt"<<endl;
+        cin>>cena;
+        cenazalitr=(cena*1000)/mililitry;
+        cout.precision(2);
+        cout<<"Cena za kilogram wynosi"<<fixed<<cenazalitr<<endl;
+    }
+    else if(wybor==3)
+    {
+        cout<<"Wybrano program numer 3"<<endl;
+        double znizka,pelnacena,procent1;
+        cout<<" Podaj %"<<endl;
+        cin>>procent1;
+        cout<<"Podaj ile kosztuje produkt"<<endl;
+        cin>>pelnacena;
+        znizka=(procent1*pelnacena)/100;
+        cout.precision(3);
+        cout<<procent1<<"%"<<" "<< "wynosi"<<" "<<fixed<<znizka<<"zl"<<endl;
+        double rabat;
+        rabat=pelnacena-znizka;
+        cout.precision(3);
+        cout<<"Cena po rabacie wyniosi "<<fixed<<rabat<<"zl"<<endl;  \\\\\\ogarn¹æ cene za kilogram
+    }
+    else if(wybor==4)
+    {
+        cout<<"Wybrano program numer 4"<<endl;
+        double cenapoczatkowa,procentznizka, produkty, cenakoncowa;
+        cout<<"Ile wynosi cena jednego produktu"<<endl;
+        cin>> cenapoczatkowa;
+        cout<<"Ile procent znizki zostanie odliczone?"<<endl;
+        cin>>procentznizka;
+        cout<<"Ile produktow musisz kupic aby otrzymac znizke"<<endl;
+        cin>>produkty;
+        cenakoncowa=(cenapoczatkowa*produkty)-(procentznizka*(cenapoczatkowa)*produkty)/100;
+        cout.precision(2);
+        cout<<"Cena koncowa wynosi"<<fixed<<cenakoncowa<<endl;
+    }
+    if(a==5)
+    {
+        cout<<"WYBRANO: 1. Liczyl jaki procent stanowi konkretna liczba"<<endl;
+        float c;
+        int b,d;
+        cout<<"Ile wynosi liczba ktora chcesz sprawdzic"<<endl;
+        cin>>b;
+        cout<<"Ile wynosi 100%"<<endl;
+        cin>>d;
+        c=(b*100)/d;
+
+        cout<<b<<" "<< "stanowi"<<" "<<fixed<<c<<"% z liczby"<<" "<<d<<endl;
+    }
+    else if(a==6)
+    {
+        cout<<" WYBRANO: 2. Ile wynosi 100%"<<endl;
+        float d;
+        int b,c;
+        cout<<"Podaj liczbe"<< endl;
+        cin>>b;
+        cout<<"Ile wynosi %"<<endl;
+        cin>>c;
+        if(b==c)
+        {
+            d=(b*100)/b;
+            cout.precision(3);
+            cout<<"100% wynosi"<<" "<<fixed<<d<<endl;
+        }
+        else
+        {
+            d=(b*100)/c;
+            cout.precision(3);
+            cout<<"100% wynosi"<<" "<<fixed<<d<<endl;
+        }
+
+
+    }
+    else if(a==7)
+    {
+        cout<<"WYBRANO: 3. Ile wynosi dany %"<<endl;
+        float b;
+        int c,d;
+        cout<<" Podaj %"<<endl;
+        cin>>c;
+        cout<<"Podaj ile wynosi 100%"<<endl;
+        cin>>d;
+        b=(c*d)/100;
+        cout.precision(3);
+        cout<<c<<"%"<<" "<< "wynosi"<<" "<<fixed<<b<<endl;
+
+    }
+
+
+}
